3d8f620f3b05c00830c3d95af59f59db65d291f5
于千皓 <916640168@qq.com>
2023-04-28 09:06:07 +0800

    鏇存柊鍐呭璇﹁鈥淩EADME.md鈥?

--- SpringEntityGenerator/MainWindow.xaml.cs
+++ SpringEntityGenerator/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace SpringEntityGenerator
 
         private void MainWindow_OnSizeChanged(object sender, SizeChangedEventArgs e)
         {
-            DataGridColumn.Height = e.NewSize.Height - 740 + 390;
+            DataGridColumn.Height = e.NewSize.Height - 740 + 320;
         }
 
         private void MainWindow_OnInitialized(object? sender, EventArgs e)
@@ -74,7 +74,7 @@ namespace SpringEntityGenerator
             var project = GetProject();
             if (project.Table.Name.Length == 0)
             {
-                MessageBox.Show("请先设置表名。", "Spring Entity Generator", MessageBoxButton.OK,MessageBoxImage.Error);
+                MessageBox.Show("请先设置表名。", "Spring Entity Generator", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
@@ -166,13 +166,58 @@ namespace SpringEntityGenerator
             ButtonGenerator.Content = "生成中，请稍后";
             try
             {
-                new MySqlGenerator().Generator(project);
-                new JavaEntityGenerator().Generator(project);
-                new JavaMapperGenerator().Generator(project);
-                new JavaServiceGenerator().Generator(project);
-                new JavaControllerGenerator().Generator(project);
-                new DocumentGenerator().Generator(project);
-                MessageBox.Show("生成成功", "Spring Entity Generator", MessageBoxButton.OK);
+                string generatorInfo = "";
+                switch (ComboBoxGeneratorType.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            new MySqlGenerator().Generator(project);
+                            new JavaEntityGenerator().Generator(project);
+                            new JavaMapperGenerator().Generator(project);
+                            new JavaServiceGenerator().Generator(project);
+                            new JavaControllerGenerator().Generator(project);
+                            new DocumentGenerator().Generator(project);
+                            generatorInfo = "数据库，JavaEntity，Java控制器，JavaMapper，JavaService，Document";
+                        }
+                        break;
+                    case 1:
+                        {
+                            new MySqlGenerator().Generator(project);
+                            generatorInfo = "数据库";
+                        }
+                        break;
+                    case 2:
+                        {
+                            new JavaControllerGenerator().Generator(project);
+                            generatorInfo = "Java控制器";
+                        }
+                        break;
+                    case 3:
+                        {
+                            new JavaEntityGenerator().Generator(project);
+                            generatorInfo = "JavaEntity";
+                        }
+                        break;
+                    case 4:
+                        {
+                            new JavaMapperGenerator().Generator(project);
+                            generatorInfo = "JavaMapper";
+                        }
+                        break;
+                    case 5:
+                        {
+                            new JavaServiceGenerator().Generator(project);
+                            generatorInfo = "JavaService";
+                        }
+                        break;
+                    case 6:
+                        {
+                            new DocumentGenerator().Generator(project);
+                            generatorInfo = "Document";
+                        }
+                        break;
+                }
+                MessageBox.Show($"生成{generatorInfo}成功", "Spring Entity Generator", MessageBoxButton.OK);
             }
             catch (Exception error)
             {
@@ -281,17 +326,17 @@ namespace SpringEntityGenerator
             if (MessageBox.Show("打开一个新的配置脚本将会丢失当前的工作进度。确定要打开新的吗？", "Spring Entity Generator", MessageBoxButton.YesNo,
                     MessageBoxImage.Question) == MessageBoxResult.Yes)
             {
-                OpenFileDialog openFileDialog=new OpenFileDialog
+                OpenFileDialog openFileDialog = new OpenFileDialog
                 {
                     Filter = "seg文件|*.seg",
                     Multiselect = false,
                     Title = "打开Spring Entity Generator文件"
                 };
-                if (openFileDialog.ShowDialog()==true)
+                if (openFileDialog.ShowDialog() == true)
                 {
                     var project = Json.Deserialize<Project>(Json.Serialize(GetProject()));
                     var table = Json.Deserialize<EntityTable>(File.ReadAllText(openFileDialog.FileName));
-                    if (table == null || project==null)
+                    if (table == null || project == null)
                     {
                         MessageBox.Show("打开失败。这个文件不是Spring Entity Generator的表结构配置文件。", "Spring Entity Generator", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
